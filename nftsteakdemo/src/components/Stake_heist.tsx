8d0e4fda6ec8afbdcf2cdeea5f8db926a2955563
Hong Beomsun <ghsqjatns@naver.com>
2022-02-09 14:42:02 +0900

    steak bar add

--- nftsteakdemo/src/components/Stake_heist.tsx
+++ nftsteakdemo/src/components/Stake_heist.tsx
@@ -7,7 +7,27 @@ import "./Stake_heist.css"
 function Stake_heist() {
     return(
         <div className="Stake_heist">
-            <p>hi</p>
+            <div className="Stake_heist_title">
+                <hr/>
+                <span>HEIST GANG</span>
+                <hr/>
+            </div>
+
+            <p>1.SLECT A HEIST YOU WANT TO JOIN THE GANG AND START TO ROB</p>
+
+            <div className="Stake_heist_connectwallet">
+                <span>CONNECT WALLET</span>
+            </div>
+
+            <div className="Stake_heist_stakeheist">
+                <div className="Stake_heist_stakeheist_progressbar">
+
+                </div>
+                
+                <div className="Stake_heist_stakeheist_button">
+                    
+                </div>
+            </div>
 
         </div>
     );
